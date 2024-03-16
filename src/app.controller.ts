4629a2f0512c301b15ac1c9cccc25bbf7c7ccd09
ksiwt <ksiwt.jp@gmail.com>
2024-03-17 00:45:26 +0900

    Chapter 07 – Index and About Pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,23 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Store',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['description'] = 'This is an about page ...';
+    viewData['author'] = 'Developed by: Your Name';
+    const data1 = 'About us - Online Store';
+    return {
+      title: data1,
+      subtitle: 'About us',
+      viewData: viewData,
+    };
+  }
 }
