dc04c6d1027fd502478dc2623cffe938b97fb604
ksiwt <ksiwt.jp@gmail.com>
2024-03-17 23:24:44 +0900

    Chapter 10 – List Products with Dummy Data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController],
 })
 export class AppModule {}
