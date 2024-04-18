4589aa538f6040bfa7d6f3855136fad371559813
ksiwt <ksiwt@icloud.com>
2024-04-18 22:47:42 +0900

    Chapter 12 – Configuration of TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'online_store',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ProductsController],
 })
 export class AppModule {}
